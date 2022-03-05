d155231a13452d623500ee0c21199be4a57ef57f
Preetam Varun <preetamarun99@gmail.com>
2022-03-05 20:52:27 +0530

    completed the challenge

---
+++ main.js
@@ -0,0 +1,21 @@
+const generateAdvice = document.getElementById("generate-advice");
+const adviceId = document.getElementById("advice-id");
+const advice = document.getElementById("advice");
+const adv = document.getElementById("adv");
+
+let getAdvice = (url) => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        advice.textContent = "";
+        advice.innerHTML += '<span>&#8220;</span>';
+        advice.textContent +=  data.slip.advice;
+        advice.innerHTML += '<span>&#8221;</span>';
+        adv.innerHTML = `Advice <span id = "advice-id">#${data.slip.id}</span>`;
+        // adviceId.textContent = "#" + data.slip.id;
+    })
+    .catch(err => (console.log(err)));
+}
+
+
+generateAdvice.addEventListener('click',() => getAdvice("https://api.adviceslip.com/advice"));
