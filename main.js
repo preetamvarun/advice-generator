aa46d0f19c0125b349b45966e62532bf8378dd9a
Preetam Varun <preetamarun99@gmail.com>
2022-03-10 16:49:21 +0530

    re-built this project using async and await

--- main.js
+++ main.js
@@ -3,17 +3,28 @@ const adviceId = document.getElementById("advice-id");
 const advice = document.getElementById("advice");
 const adv = document.getElementById("adv");
 
-let getAdvice = (url) => {
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        advice.textContent = "";
-        advice.innerHTML += '<span>&#8220;</span>';
-        advice.textContent +=  data.slip.advice;
-        advice.innerHTML += '<span>&#8221;</span>';
-        adv.innerHTML = `Advice <span id = "advice-id">#${data.slip.id}</span>`;
-    })
-    .catch(err => (console.log(err)));
+// let getAdvice = (url) => {
+//     fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         advice.textContent = "";
+//         advice.innerHTML += '<span>&#8220;</span>';
+//         advice.textContent +=  data.slip.advice;
+//         advice.innerHTML += '<span>&#8221;</span>';
+//         adv.innerHTML = `Advice <span id = "advice-id">#${data.slip.id}</span>`;
+//     })
+//     .catch(err => (console.log(err)));
+// }
+
+
+let getAdvice = async(url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    advice.textContent = "";
+    advice.innerHTML += '<span>&#8220;</span>';
+    advice.textContent +=  data.slip.advice;
+    advice.innerHTML += '<span>&#8221;</span>';
+    adv.innerHTML = `Advice <span id = "advice-id">#${data.slip.id}</span>`;
 }
 
 
